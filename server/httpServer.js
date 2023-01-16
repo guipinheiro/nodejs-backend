022a6bfeedd93db01b28d6bf73e85c3d3349bcbf
Guilherme Pinheiro <gui.pinheiro@outlook.com>
2023-01-16 14:48:58 -0300

    Created basic http server with different webpages

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,10 +1,13 @@
 const http = require("http");
+const slugify = require("slugify");
 
 const server = http.createServer((req, res) => {
 	if (req.url === "/") {
-		res.end("<h1>Home</h1>");
+		res.end("<h1>Home 2</h1>");
 	} else if (req.url === "/about") {
 		res.end("<h1>About</h1>");
+	} else if (req.url === "/slug") {
+		res.end(`<h1>${slugify("slufied webpage title")}</h1>`);
 	} else {
 		res.writeHead(404, {
 			"Content-type": "text/html",
